255619a5b21658d5a5148b83c85fa5da5a5997a8
sbigfoot13 <seiya-oashi@agilecore.co.jp>
2023-05-18 11:05:52 +0900

    整理整頓

---
+++ ToDoソフト/JS/ToDosoft.js
@@ -0,0 +1,29 @@
+const form = document.querySelector(".form");
+const taskInput = document.querySelector(".task-input");
+const todoList = document.querySelector(".todo-list");
+
+let state = {
+  tasks: [],
+};
+
+// Dynamic HTML template for new tasks
+const template = (task) =>
+  `<div>
+    <p>${task.task}</p>
+  </div>`;
+
+// Render the template to the DOM
+const render = (htmlString, el) => {
+  el.innerHTML += htmlString;
+};
+
+// Submit form
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const task = {
+    task: taskInput.value,
+  };
+  state.tasks = [...state.tasks, task];
+  render(template(state.tasks[state.tasks.length - 1]), todoList);
+  taskInput.value = "";
+});
