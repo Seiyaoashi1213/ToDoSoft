125b9b82d215fc156a3ebcba34087e7e7547f9b6
sbigfoot13 <seiya-oashi@agilecore.co.jp>
2023-05-19 15:07:02 +0900

    定数定義まで終了

--- ToDoソフト/JS/ToDosoft.js
+++ ToDoソフト/JS/ToDosoft.js
@@ -1,32 +1,42 @@
 const form = document.querySelector(".form");
-const entryDay = document.querySelector(".entry-day");
-const out = document.querySelector(".out");
-const color = document.querySelector(".color");
+
+const entryDay = document.querySelector(".entry");
+const outDay = document.querySelector(".out");
+
 const contentTitle = document.querySelector(".sub-title");
-const content = document.querySelector("textarea");
-const input = [entryDay, out, contentTitle, content];
+const contet = document.querySelector(textarea);
+const allContent = [
+  entryDay.value,
+  outDay.value,
+  contentTitle.value,
+  contet.value,
+];
+
+const taskInput = document.querySelector(".task-input");
 const todoList = document.querySelector(".todo-list");
 
 let state = {
   tasks: [],
 };
 
-const template = (taskInput) =>
-  `
-  <tr>
-    <th>
-      <p>${taskInput}</p>
-    </th>
-  </tr>;
-  `;
+// Dynamic HTML template for new tasks
+const template = (task) =>
+  `<div>
+    <p>${task.task}</p>
+  </div>`;
 
+// Render the template to the DOM
+const render = (htmlString, el) => {
+  el.innerHTML += htmlString;
+};
+
+// Submit form
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const taskInput = {
-    taskInput: input.value,
+  const task = {
+    task: taskInput.value,
   };
-
-  state.tasks = [...state.tasks, taskInput];
+  state.tasks = [...state.tasks, task];
   render(template(state.tasks[state.tasks.length - 1]), todoList);
-  form.value = "";
+  taskInput.value = "";
 });
