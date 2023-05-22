dc95d9d0bc6918957ecdf077815e1f6f39a49f9c
sbigfoot13 <seiya-oashi@agilecore.co.jp>
2023-05-22 11:45:57 +0900

    山田さんのおかげでタスク表示するところまで何とか進みました

--- ToDoソフト/JS/ToDosoft.js
+++ ToDoソフト/JS/ToDosoft.js
@@ -1,16 +1,17 @@
 const form = document.querySelector(".form");
 
-const entryDay = document.querySelector(".entry");
-const outDay = document.querySelector(".out");
+const entryYear = document.querySelector(".entry-year");
+const entryMonth = document.querySelector(".entry-month");
+const entryDay = document.querySelector(".entry-day");
+
+const outYear = document.querySelector(".out-year");
+const outMonth = document.querySelector(".out-month");
+const outDay = document.querySelector(".out-day");
 
 const contentTitle = document.querySelector(".sub-title");
-const contet = document.querySelector("textarea");
-const allContent = [
-  entryDay.value,
-  outDay.value,
-  contentTitle.value,
-  contet.value,
-];
+const content = document.querySelector('textarea[name="content"]');
+
+const backgroundColor = document.querySelector(".color");
 
 const todoList = document.querySelector(".todo-list");
 
@@ -20,9 +21,26 @@ let state = {
 
 // Dynamic HTML template for new tasks
 const template = (task) =>
-  `<th>
-    <p>${task.task}</p>
-  </th>`;
+  `<tr>
+    <div class="all-task">
+      <div class="all-days">
+        <div class="entry">
+          <p>記入</p>
+          <p>${task.entryYear}年</p>
+          <p>${task.entryMonth}</p>
+          <p>${task.entryDay}</p>
+        </div>
+        <div class="end">
+          <p>期日</p>
+          <p>${task.outYear}年</p>
+          <p>${task.outMonth}</p>
+          <p>${task.outDay}</p>
+        </div>
+      </div>
+      <p>${task.title}</p>
+      <p>${task.content}</p>
+    </div>
+  </tr>`;
 
 // Render the template to the DOM
 const render = (htmlString, el) => {
@@ -33,9 +51,17 @@ const render = (htmlString, el) => {
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const task = {
-    task: allContent.value,
+    entryYear: entryYear.value,
+    entryMonth: entryMonth.value,
+    entryDay: entryDay.value,
+    outYear: outYear.value,
+    outMonth: outMonth.value,
+    outDay: outDay.value,
+    title: contentTitle.value,
+    content: content.value,
   };
+
   state.tasks = [...state.tasks, task];
   render(template(state.tasks[state.tasks.length - 1]), todoList);
-  form.value = "";
+  contentTitle.value = "";
 });
